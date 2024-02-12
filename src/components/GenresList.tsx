954f4334b3bb517dc15acabab0f8a664ad0352ed
unknown <jbins9719@gmail.com>
2024-02-12 17:45:58 +0330

    fixed the fontsize

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -9,7 +9,7 @@ const GenresList = () => {
         <ListItem key={genre.id}>
           <HStack paddingTop={2}>
             <Image boxSize="42px" borderRadius="8px" src={getUrlOfLoadedImages(genre.image_background)}></Image>
-            <Text>{genre.name}</Text>
+            <Text fontSize={19}>{genre.name}</Text>
           </HStack>
 
         </ListItem>
