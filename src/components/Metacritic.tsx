fb0164cb148f789c7bda126c786ba77d0d0cb793
unknown <jbins9719@gmail.com>
2024-02-14 18:44:04 +0330

    done

--- src/components/Metacritic.tsx
+++ src/components/Metacritic.tsx
@@ -4,7 +4,7 @@ interface Props {
     score: number;
 }
 const Metacritic = ({score }: Props) => {
-    const color = score > 85 ? "green" : score > 65 ? 'yellow' : '';
+    const color = score > 85 ? "green" : score > 65 ? 'yellow' : score < 65 ? 'red' : '';
   return (
     <Badge borderRadius={5} colorScheme={color} fontSize={18} paddingX={2}>
       {score}
