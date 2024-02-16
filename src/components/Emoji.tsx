972e74a94c6d7a0274c25a99f9db22c615dad90d
unknown <jbins9719@gmail.com>
2024-02-16 10:41:32 +0330

    Fixed the rating

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,21 @@
+import Thumb from '../assets/thumbs-up.238x256.png'
+import Meh from '../assets/neutral-face.256x256.png'
+import Bulleye from '../assets/bullseye.svg'
+import { Image, ImageProps } from '@chakra-ui/react'
+interface Props {
+    rating: number;
+}
+
+const Emoji = ({rating}:Props) => {
+    if(rating < 3) return null;
+    const rates:{[key: number]:ImageProps} = {
+        3:{src: Thumb, alt: 'recommended'},
+        4:{src: Meh, alt: 'not good'},
+        5:{src: Bulleye, alt: 'exelent'},
+    }
+    return (
+        <Image {...rates[rating]}/>
+    )
+}
+
+export default Emoji
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,21 +3,24 @@ import { Game } from "../hooks/useGame";
 import PlatformsIconList from "./PlatformsIconList";
 import Metacritic from "./Metacritic";
 import getUrlOfLoadedImages from "../services/image-crop";
+import Emoji from "./Emoji";
 interface Props {
   game: Game;
 }
+
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
       <Image src={(getUrlOfLoadedImages(game.background_image))} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformsIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <Metacritic score={game.metacritic} />
         </HStack>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Emoji rating={game.rating_top}/>
       </CardBody>
     </Card>
   );
