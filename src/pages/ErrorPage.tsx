c781ec4832efda9c782cf397fbad7e133fe9a8b9
james <jbins9719@gmail.com>
2024-09-30 18:23:25 +0330

    fixxed

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,20 +1,9 @@
-import { useRouteError } from "react-router-dom";
-import { isRouteErrorResponse } from "react-router-dom";
 import NavBar from "../components/NavBar";
 const ErrorPage = () => {
-  const error = useRouteError();
   return (
     <div>
       <div>
         <NavBar />
-      </div>
-      <div className="ms-3">
-        <h1>Oops!!.. Sorry</h1>
-        <p>
-          {isRouteErrorResponse(error)
-            ? "This Page is Invalid"
-            : "You Have Runned Into an Error!!"}
-        </p>
       </div>
     </div>
   );
